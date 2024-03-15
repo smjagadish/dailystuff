b1428baeb64225fd2407bfbb1eb9d5d838f192f6
S.M. Jagadish <smjagadish@gmail.com>
2024-03-14 20:46:40 -0400

    updated global task pool with a recurring no data task entry

--- til/Mar24/Mar11/spring_db_scheduler/src/main/java/org/taskDefinition/taskConstants.java
+++ til/Mar24/Mar11/spring_db_scheduler/src/main/java/org/taskDefinition/taskConstants.java
@@ -2,6 +2,8 @@ package org.taskDefinition;
 
 import com.github.kagkarlsson.scheduler.task.*;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
+import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
+import com.github.kagkarlsson.scheduler.task.schedule.Schedule;
 import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +23,7 @@ public class taskConstants {
     public Task<?>[] returnMap()
     {
 
-        Task<?>[] task_array = new Task<?>[2];
+        Task<?>[] task_array = new Task<?>[3];
 
             task_array[0] = Tasks.oneTime("one_time_task_nd")
                     .execute(new VoidExecutionHandler<Void>() {
@@ -35,6 +37,13 @@ public class taskConstants {
                                 @Override
                                 public void execute(TaskInstance<Integer> taskInstance, ExecutionContext executionContext) {
                                    System.out.println("logging the data with seed value:"+taskInstance.getData());
+                                }
+                            });
+            task_array[2] = Tasks.recurring("recurring_common_nd", FixedDelay.ofMinutes(4))
+                            .execute(new VoidExecutionHandler<Void>() {
+                                @Override
+                                public void execute(TaskInstance<Void> taskInstance, ExecutionContext executionContext) {
+                                   System.out.println("logs from a common recur task with no data");
                                 }
                             });
 
